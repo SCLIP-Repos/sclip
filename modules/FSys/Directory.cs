67f121c4acff7818bf378e95196ba5c6b59f574e
Ryu-ji <>
2018-02-11 23:33:53 +0900

    Add Directory.cs

---
+++ modules/FSys/Directory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace FSys
+{
+    public class FSysDirectory
+    {
+        public FSysDirectory() => Excepter.Clear();
+
+        static FSysDirectory() => Excepter.Clear();
+
+
+
+        public static void MkDir(string DirectoryPath)
+        {
+            if (!Directory.Exists(DirectoryPath))
+            {
+                try
+                {
+                    Directory.CreateDirectory(DirectoryPath);
+                }
+                catch (IOException ex)
+                {
+                    Excepter.Except = ex.ToString();
+
+                }
+            }
+            else
+            {
+                Excepter.Err = "Directory already exists.";
+            }
+        }
+
+
+
+    }
+
+}
