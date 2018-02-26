6cf3feb86ed22319630fdd2825203b6207e0b692
Ryu-ji <ryuji.mail.g@gmail.com>
2018-02-27 01:25:23 +0900

    test

---
+++ app/Windows/Security/Login.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace $safeprojectname$.Security
+{
+    public class Login
+    {
+        public static string LoginKey;
+
+
+        private class InternalEncrypt
+        {
+            public static string InternalKey { get; private set; }
+
+
+
+            private string Key, Iv;
+
+            public InternalEncrypt()
+            {
+                InternalKey = null;
+
+
+            }
+        }
+    }
+}
