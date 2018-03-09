dc8ee91eba89439fe3ea50123a8d7a652edbbc1d
Ryu-ji <ryuji.mail.g@gmail.com>
2018-03-09 15:39:29 +0900

    update

--- app/Windows/Sets.Designer.cs
+++ app/Windows/Sets.Designer.cs
@@ -35,7 +35,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(574, 541);
+            this.ClientSize = new System.Drawing.Size(774, 564);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Sets";
             this.Text = "Settings";
