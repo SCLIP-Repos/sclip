8bd5c8788ee9d6babf45465cea07abbf2480980e
ITechJPN <itech-github@itech.jpn.org>
2018-04-15 15:39:04 +0900

    up

--- app/windows/Form1.Designer.cs
+++ app/windows/Form1.Designer.cs
@@ -37,8 +37,12 @@
             this.Delete_button = new System.Windows.Forms.ToolStripMenuItem();
             this.Logout_button = new System.Windows.Forms.ToolStripButton();
             this.Config_button = new System.Windows.Forms.ToolStripButton();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.Item_listBox = new System.Windows.Forms.ListBox();
+            this.Base_panel = new System.Windows.Forms.Panel();
             this.tableLayoutPanel1.SuspendLayout();
             this.toolStrip1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -48,6 +52,7 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 849F));
             this.tableLayoutPanel1.Controls.Add(this.toolStrip1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -133,6 +138,42 @@
             this.Config_button.Text = "Config";
             this.Config_button.Click += new System.EventHandler(this.Config_button_Click);
             // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9.386973F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 90.61303F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.Controls.Add(this.Item_listBox, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.Base_panel, 1, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 34);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(1044, 534);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // Item_listBox
+            // 
+            this.Item_listBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Item_listBox.FormattingEnabled = true;
+            this.Item_listBox.ItemHeight = 12;
+            this.Item_listBox.Location = new System.Drawing.Point(3, 3);
+            this.Item_listBox.Name = "Item_listBox";
+            this.Item_listBox.Size = new System.Drawing.Size(92, 528);
+            this.Item_listBox.TabIndex = 0;
+            // 
+            // Base_panel
+            // 
+            this.Base_panel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Base_panel.Location = new System.Drawing.Point(101, 3);
+            this.Base_panel.Name = "Base_panel";
+            this.Base_panel.Size = new System.Drawing.Size(940, 528);
+            this.Base_panel.TabIndex = 1;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
@@ -149,6 +190,7 @@
             this.tableLayoutPanel1.PerformLayout();
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -163,6 +205,9 @@
         private System.Windows.Forms.ToolStripMenuItem Delete_button;
         private System.Windows.Forms.ToolStripButton Config_button;
         private System.Windows.Forms.ToolStripButton Logout_button;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.ListBox Item_listBox;
+        private System.Windows.Forms.Panel Base_panel;
     }
 }
 
