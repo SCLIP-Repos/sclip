cd7e634cc99b56e279fc0c97eaa5a076119e5ce3
ITechJPN <itech-github@itech.jpn.org>
2018-04-18 14:26:46 +0900

    update

---
+++ app/windows/View/Config/Confs.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace SCLIP.View.Config
+{
+    partial class Confs
+    {
+        /// <summary> 
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region コンポーネント デザイナーで生成されたコード
+
+        /// <summary> 
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を 
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(522, 508);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(514, 482);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Passowrd";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Confs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Confs";
+            this.Size = new System.Drawing.Size(522, 508);
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
---
+++ app/windows/View/Config/Setup/Agree.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace SCLIP.View.Config.Setup
+{
+    partial class Agree
+    {
+        /// <summary> 
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region コンポーネント デザイナーで生成されたコード
+
+        /// <summary> 
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を 
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Agree));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.NEXT_button = new System.Windows.Forms.Button();
+            this.Agree_checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.White;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(522, 453);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            // 
+            // NEXT_button
+            // 
+            this.NEXT_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.NEXT_button.Location = new System.Drawing.Point(428, 459);
+            this.NEXT_button.Name = "NEXT_button";
+            this.NEXT_button.Size = new System.Drawing.Size(91, 38);
+            this.NEXT_button.TabIndex = 4;
+            this.NEXT_button.Text = "NEXT";
+            this.NEXT_button.UseVisualStyleBackColor = true;
+            // 
+            // Agree_checkBox1
+            // 
+            this.Agree_checkBox1.AutoSize = true;
+            this.Agree_checkBox1.Location = new System.Drawing.Point(368, 459);
+            this.Agree_checkBox1.Name = "Agree_checkBox1";
+            this.Agree_checkBox1.Size = new System.Drawing.Size(54, 16);
+            this.Agree_checkBox1.TabIndex = 5;
+            this.Agree_checkBox1.Text = "Agree";
+            this.Agree_checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Agree
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.Agree_checkBox1);
+            this.Controls.Add(this.NEXT_button);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Agree";
+            this.Size = new System.Drawing.Size(522, 500);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button NEXT_button;
+        private System.Windows.Forms.CheckBox Agree_checkBox1;
+    }
+}
