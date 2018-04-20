eb150dc6ef068cc5c90f0736e2d4b0a8e5fe7a23
ITechJPN <itech-github@itech.jpn.org>
2018-04-20 15:07:37 +0900

    update

--- app/windows/View/Config/Setup/Passwd.cs
+++ app/windows/View/Config/Setup/Passwd.cs
@@ -49,6 +49,65 @@ namespace SCLIP.View.Config.Setup
             }
             */
 
+
+            char[] tmp =
+            {
+                '!',
+                '?',
+                '*',
+                '+',
+                '@',
+                '"',
+                '`',
+                '#',
+                '$',
+                '%',
+                '&',
+                '\\' ,
+                '=',
+                '-',
+                '(',
+                ')',
+                '{',
+                '}',
+                '~',
+                '^',
+                '|'
+            };
+
+            GeneralPurpose.PasswdCheck passwdCheck = new GeneralPurpose.PasswdCheck(Passwd_textBox.Text);
+            
+            if(passwdCheck.Length(5))
+            {
+                Strength_progressBar.Value = 10;
+                Strength_label.Text = Properties.Resources.password_Strength_Weak;
+            }
+
+            if (passwdCheck.Length(5) && passwdCheck.Check("[a-z]") && passwdCheck.Check("[A-Z]"))
+            {
+                Strength_progressBar.Value = 20;
+                Strength_label.Text = Properties.Resources.password_Strength_Weak;
+            }
+
+            if (passwdCheck.Length(6) && passwdCheck.Check("[a-z]") && passwdCheck.Check("[A-Z]"))
+            {
+                Strength_progressBar.Value = 30;
+                Strength_label.Text = Properties.Resources.password_Strength_Weak;
+            }
+
+            if (passwdCheck.Length(6) && passwdCheck.Check("[a-z]") && passwdCheck.Check("[A-Z]") && passwdCheck.isChar(tmp))
+            {
+                Strength_progressBar.Value = 50;
+                Strength_label.Text = Properties.Resources.password_Strength_LittleNice;
+            }
+
+
+            if (passwdCheck.Length(8) && passwdCheck.Check("[a-z]") && passwdCheck.Check("[A-Z]") && passwdCheck.isChar(tmp))
+            {
+                Strength_progressBar.Value = 100;
+                Strength_label.Text = Properties.Resources.password_Strength_Strong;
+            }
+
         }
 
         private void NEXT_button_Click(object sender, EventArgs e)
