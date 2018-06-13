6e512ec3d3ee351693d47d66baf488e1b349f84d
Ryu-ji <>
2018-06-13 16:35:11 +0900

    new

---
+++ mobile/ios/src/ViewController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+using UIKit;
+
+namespace SCLIP.iOS
+{
+    public partial class ViewController : UIViewController
+    {
+
+        /*
+        partial void UIButton204_TouchUpInside(UIButton sender)
+        {
+            var a = new UIAlertView("Title", "Hello", null, "OK", "CANCEL");
+
+
+            a.Show();
+        }
+
+        */
+
+
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            // Code to start the Xamarin Test Cloud Agent
+#if ENABLE_TEST_CLOUD
+			Xamarin.Calabash.Start ();
+#endif
+
+
+
+
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.		
+        }
+    }
+}
