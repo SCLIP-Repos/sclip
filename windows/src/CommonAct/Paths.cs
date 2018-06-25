ff8155e7da47be233c49b48360c19e65001ccbc6
Ryu-ji <ryuji.mail.g@gmail.com>
2018-06-26 01:32:34 +0900

    Add path.cs

--- windows/src/CommonAct/Paths.cs
+++ windows/src/CommonAct/Paths.cs
@@ -8,5 +8,36 @@ namespace SCLIP.CommonAct
 {
     class Paths
     {
+        internal readonly static string[] DirectoryPaths =
+        {
+            //0
+            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\sclip\",
+
+            //1
+            DirectoryPaths[0] + @"set\",
+
+            //2
+            DirectoryPaths[0] + @"logs\",
+
+            //3
+            Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\sclip\",
+
+            //4
+            DirectoryPaths[3] + @"save\",
+
+            //5
+            DirectoryPaths[3] + @"trash\"
+        };
+
+        internal readonly static string[] FilePaths =
+        {
+            DirectoryPaths[1] + "sclip.json",
+
+            DirectoryPaths[1] + "authentication.json",
+
+            DirectoryPaths[2] + "setup.log",
+
+            DirectoryPaths[2] + "authentication.log"
+        };
     }
 }
